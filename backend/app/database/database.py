00c7f616cf88b90b7ce91b67e03e7b10f28824b0
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-14 22:24:11 +0530

    R

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,4 +1,4 @@
-from connection import SessionLocal
+from app.database.connection import SessionLocal
 
 def get_db():
     db = SessionLocal()
--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import users, rooms, bookings, payments, auth
-
-# Import database engine and models
-from app.database.connection import engine,Base
-
+from app.routers import users, rooms, bookings, payments, auth, ai
+from app.database.connection import engine, Base
 
 from app.models.user import User
 from app.models.room import Room
 from app.models.booking import Booking
 from app.models.payment import Payment
 
-# Create all tables
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="Hotel Management System")
 
 origins = [
     "http://localhost:5173"
@@ -33,8 +29,9 @@ app.add_middleware(
 def home():
     return {"message": "Backend Working"}
 
+app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(rooms.router)
 app.include_router(bookings.router)
 app.include_router(payments.router)
-app.include_router(auth.router)
+app.include_router(ai.router)
